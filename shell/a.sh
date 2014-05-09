e24744379a151b0afd07acdc604acf72d9577650
Luis <laureyap@yahoo.com>
2014-05-10 01:23:17 +0800

    add some shell script file

---
+++ shell/a.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true
+do
+    read -p "PLS write your name:" username
+    read -p "PLS write your age:" userage
+    echo -e "your name is : $username & your age is: $userage \n"
+break
+done
---
+++ shell/b.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Program Desc:
+#       This program shows "Hello World!" in your screen.
+# History:
+echo "Hello World! \a \n"
+exit 0
