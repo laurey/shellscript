cdb1e92ab1c9208a6d42d8487f948098f3337028
Luis <laureyap@yahoo.com>
2014-05-22 00:17:21 +0800

    test uppercase or lowercase in bash shell

--- apt.sh
+++ apt.sh
@@ -4,6 +4,8 @@ do
     read -p "PLS enter one pid :" pid
     echo -e "you enter pid is : $pid \n"
     read -p "Update your APT packages(Y/N):" _update
+    _update=${_update^^}
+    echo -e "the input update in uppercase: $_update \n"
     if [ "$_update" != "N" ] && [ "$_update" != "n" ]; then
         _update="Y"
     else
