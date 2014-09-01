eb5f044a4243713a6f27de01202363b671c33202
Luis <laureyap@yahoo.com>
2014-09-02 00:54:35 +0800

    add -an param

--- mplayer.test.sh
+++ mplayer.test.sh
@@ -2,7 +2,7 @@
 x=0
 while true
     do
-        if [ -e $1 ]; then
+        if [[ -e $1 ]]; then
             videopath=$1
         else
             read -p "PLS write your video path: " videopath
@@ -21,6 +21,12 @@ while true
             #exit 1
         fi
         
+        # 输出视频文件基本信息 
+        # mediainfo MY.TEST.VIDEO.mp4
+        
+        # 输出音频文件时长和比特率
+        # mediainfo --Inform="Audio;%Duration% %BitRate%" MY.TEST.AUDIO.mp3
+
         # example
         # mplayer -identify -nosound -vc dummy -vo null MY.TEST.VIDEO.mp4
         /usr/bin/mplayer -identify -nosound -vc dummy -vo null $videopath
