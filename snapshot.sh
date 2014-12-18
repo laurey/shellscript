f5271e8461e08794a24ae34f1f5c4e15119e100b
laurey <laurey@yahoo.com>
2014-12-18 15:21:35 +0800

    代码修正

--- snapshot.sh
+++ snapshot.sh
@@ -28,25 +28,25 @@ while true
 
         # example 
         # 提取视频文件中的部分 
-        # ffmpeg -i They-Don\'t-Care-About-Us.mp4 -ss 00:09:03 -map 0 -b:a 128k -acodec copy -vcodec copy -t 00:05:55 fragment.mp4
+        # ffmpeg -ss 00:09:03 -i They-Don\'t-Care-About-Us.mp4 -map 0 -b:a 128k -acodec copy -vcodec copy -t 00:05:55 fragment.mp4
         #
         #
         # example 
         # 将视频中任意一帧保存为指定宽高图片 
-        # ffmpeg -i They-Don\'t-Care-About-Us.mp4 -ss 89 -y -f image2 -s 1280x720 -vframes 1 -an snap1.png 
+        # ffmpeg -ss 89 -i They-Don\'t-Care-About-Us.mp4 -y -f image2 -s 1280x720 -vframes 1 -an snap1.png 
         #
         # 将视频中起始时间为M的N帧保存为指定宽高的N张图片 
-        # ffmpeg -i They-Do-not-Care-About-Us.mp4 -ss 89 -y -f image2 -s 1280x720 -vframes 100 -an snap-%03d.png 
+        # ffmpeg -ss 89 -i They-Do-not-Care-About-Us.mp4 -y -f image2 -s 1280x720 -vframes 100 -an snap-%03d.png 
         #
         #
         # 将视频中N帧保存为指定宽高的GIF动画 
         # ffmpeg -i yourvideopath -vframes N -y -f gif -s widthxheight -an outputfilename
-        # ffmpeg -i Dragon.Ball.Kai.mp4 -ss 550 -vframes 188 -y -f gif -s 1280x720 -an animate.gif
+        # ffmpeg -ss 550 -i Dragon.Ball.Kai.mp4 -vframes 188 -y -f gif -s 1280x720 -an animate.gif
 
         outputfile="~/Desktop/"$(date +%Y%m%d%H%M%S)".png"
-        # command="ffmpeg -i $videopath -ss 188 -y -f image2 -s 720x480 -vframes 1 -an $outputfile"
+        # command="ffmpeg -ss 199 -i $videopath -y -f image2 -s 720x480 -vframes 1 -an $outputfile"
 
-        command="ffmpeg -i $videopath -ss 188 -y -f image2 -s 720x480 -vframes 1 -an $outputfile"
+        command="ffmpeg -ss 199 -i $videopath -y -f image2 -s 720x480 -vframes 1 -an $outputfile"
         echo $command
         eval $command
        break
